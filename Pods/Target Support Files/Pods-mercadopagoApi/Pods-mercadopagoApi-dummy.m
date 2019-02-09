c51b89180fb9589f06442ac1f68bd0243c0747f1
Roberto <garciamercado.r@gmail.com>
2019-02-09 19:37:25 -0300

    init with cocoapods

---
+++ Pods/Target Support Files/Pods-mercadopagoApi/Pods-mercadopagoApi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mercadopagoApi : NSObject
+@end
+@implementation PodsDummy_Pods_mercadopagoApi
+@end
---
+++ Pods/Target Support Files/Pods-mercadopagoApiTests/Pods-mercadopagoApiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mercadopagoApiTests : NSObject
+@end
+@implementation PodsDummy_Pods_mercadopagoApiTests
+@end
